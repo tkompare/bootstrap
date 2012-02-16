61613cdedef71defca611ffcca17f3afec6ceca9
tkompare <tom@kompare.us>
2012-02-15 18:21:15 -0600

    [bootstrap] more playing...

---
+++ public/index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Bootstrap, from Twitter</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	
+	<!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	
+	<!-- Le styles -->
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<style type="text/css">
+		body {
+			padding-top: 60px;
+			padding-bottom: 30px;
+		}
+	</style>
+	<link href="css/bootstrap-responsive.css" rel="stylesheet">
+	
+	<!-- Le fav and touch icons -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+	
+	<!-- Le Google Javascript API -->
+	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+</head>
+
+<body>
+	
+	<div class="navbar navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container-fluid">
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+					<span class="icon-bar"></span> 
+					<span class="icon-bar"></span> 
+					<span class="icon-bar"></span> 
+				</a>
+				<a class="brand" href="#">311 Servic.es</a>
+				<div class="nav-collapse">
+					<ul class="nav">
+						<li id="linav-home" class="active"><a id="thenav-home">Home</a></li>
+						<li id="linav-location"><a id="thenav-location">Location</a></li>
+						<li id="linav-service"><a id="thenav-service">Service</a></li>
+						<li id="linav-contact"><a id="thenav-contact">Contact</a></li>
+					</ul>
+				</div>
+				<!--/.nav-collapse -->
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row-fluid">
+			<div class="span12">
+				<div id="section-home" class="hero-unit">
+					<h2>Chicago 311 Services</h2>
+					<ol>
+						<li>Find Your Location</li>
+						<li>Choose Request Type</li>
+						<li>Give Us The Details</li>
+						<li>Enter Your Contact Information For Follow-Up</li>
+					</ol>
+					<p><a id="start" class="btn btn-primary btn-small">Start &raquo;</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div id="section-location" class="row-fluid">
+			<div class="span12">
+				<div class="row-fluid">
+					<div id="heading-map" class="span6">
+						<h2>The Map</h2>
+						<div id="map-themap" style="width:100%; height:200px">
+						<!-- The Google map goes here. -->
+						</div>
+						<p>A container that holds a google map will go here</p>
+						<p><a id="uselocation" class="btn btn-small">Use this location &raquo;</a></p>
+					</div>
+					<!--/span-->
+					<div id="heading-address" class="span6">
+						<h2>Address</h2>
+						<p>Form elements that take in address information</p>
+						<p><a id="seemap" class="btn btn-small">See Map &raquo;</a></p>
+					</div>
+					<!--/span-->
+				</div>
+			</div>
+		</div>
+		<div id="section-service" class="row-fluid">
+			<div class="span12">
+				<div class="row-fluid">
+					<div class="span6">
+						<h2>Heading</h2>
+						<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+						tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum
+						massa justo sit amet risus. Etiam porta sem malesuada magna mollis
+						euismod. Donec sed odio dui.</p>
+						<p><a class="btn" href="#">View details &raquo;</a></p>
+					</div>
+					<!--/span-->
+					<div class="span6">
+						<h2>Heading</h2>
+						<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+						tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum
+						massa justo sit amet risus. Etiam porta sem malesuada magna mollis
+						euismod. Donec sed odio dui.</p>
+						<p><a class="btn" href="#">View details &raquo;</a></p>
+					</div>
+					<!--/span-->
+				</div>
+			</div>
+		</div>
+		<div id="section-contact" class="row-fluid">
+			<div class="span12">
+				<div class="row-fluid">
+					<div class="span6">
+						<h2>Heading</h2>
+						<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+						tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum
+						massa justo sit amet risus. Etiam porta sem malesuada magna mollis
+						euismod. Donec sed odio dui.</p>
+						<p><a class="btn" href="#">View details &raquo;</a></p>
+					</div>
+					<!--/span-->
+					<div class="span6">
+						<h2>Heading</h2>
+						<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+						tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum
+						massa justo sit amet risus. Etiam porta sem malesuada magna mollis
+						euismod. Donec sed odio dui.</p>
+						<p><a class="btn" href="#">View details &raquo;</a></p>
+					</div>
+					<!--/span-->
+				</div>
+				<!--/row-->
+			</div>
+			<!--/span-->
+		</div>
+		<!--/row-->
+		<hr>
+		<footer>
+			<p>&copy; Company 2012</p>
+		</footer>
+	</div>
+	<!--/.fluid-container-->
+	
+	<!-- Le javascript
+	    ================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="https://maps-api-ssl.google.com/maps/api/js?sensor=true" type="text/javascript"></script>
+	<script src="js/maps.google.polygon.containsLatLng.js" type="text/javascript"></script>
+	<script src="js/49.js" type="text/javascript"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
---
+++ public/js/main.js
@@ -0,0 +1,285 @@
+$(document).ready(function() {
+	$("#section-location,#section-service,#section-contact,#heading-map").hide();
+	$("#thenav-home").click(function(){
+		$("#section-location,#section-service,#section-contact").hide("fast");
+		$("#section-home").show("fast");
+		$("#linav-location,#linav-service,#linav-contact").removeClass('active');
+		$("#linav-home").addClass('active');
+		$(".nav-collapse").collapse('hide');
+	});
+	$("#start,#thenav-location").click(function() {
+		$("#section-home,#section-service,#section-contact").hide("fast");
+		$("#section-location").show("fast");
+		$("#linav-home,#linav-service,#linav-contact").removeClass('active');
+		$("#linav-location").addClass('active');
+		$(".nav-collapse").collapse('hide');
+	});
+	$("#toservice,#thenav-service").click(function() {
+		$("#section-home,#section-location,#section-contact").hide("fast");
+		$("#section-service").show("fast");
+		$("#linav-home,#linav-location,#linav-contact").removeClass('active');
+		$("#linav-service").addClass('active');
+		$(".nav-collapse").collapse('hide');
+	});
+	$("#tocontact,#thenav-contact").click(function() {
+		$("#section-home,#section-location,#section-service").hide("fast");
+		$("#section-contact").show("fast");
+		$("#linav-home,#linav-location,#linav-service").removeClass('active');
+		$("#linav-contact").addClass('active');
+		$(".nav-collapse").collapse('hide');
+	});
+	$("#seemap").click(function() {
+		$("#heading-map").show("fast", function(){
+			loadMap('map-themap');
+		});
+	});
+	$("#uselocation").click(function() {
+		$("#heading-map").hide("fast");
+	});
+	function loadMap(mapdomobject)
+	{
+		var theAddress = null;
+		/*
+		 * If the user's browser understands how to try to get location information...
+		 */
+		if(navigator.geolocation)
+		{
+			/*
+			 * Try to get the latest geolocation data from the user's device
+			 */
+			navigator.geolocation.getCurrentPosition
+			(
+				/*
+				 * If the device returns a position...
+				 */
+				function(Position)
+				{
+					/*
+					 * Make the map
+					 */
+					var TheMap = new google.maps.Map
+					(
+						/*
+						 * Put the map in the correct element in the DOM
+						 */
+						document.getElementById(mapdomobject),
+						{
+							/*
+							 * Make the map a road map
+							 */
+							mapTypeId: google.maps.MapTypeId.ROADMAP,
+							/*
+							 * Don't show map controls that we don't want
+							 * to appear. This web site is for small screen
+							 * devices; mostly mobile phones. Don't clutter
+							 * the screen.
+							 */
+							mapTypeControl: false,
+							streetViewControl: false,
+							panControl: false,
+							/*
+							 * Zoom level 17 seems like a good generic start level.
+							 */
+							zoom: 17,
+							/* 
+							 * Zoom control buttons needed for all (some?) Android
+							 * devices as t2hey don't do un/pinch for un/zooming.
+							 * For consistency through all devices, let's just
+							 * show them on all devices.
+							 */
+							zoomControl: true,
+							zoomControlOptions:
+							{
+								style: google.maps.ZoomControlStyle.SMALL,
+								position: google.maps.ControlPosition.LEFT_TOP
+							}
+						}
+					);
+					/*
+					 * Create the location object
+					 */
+					var MyLocation = new google.maps.LatLng(Position.coords.latitude,Position.coords.longitude);
+					/*
+					 * Center the map on the user's location
+					 */
+					TheMap.setCenter(MyLocation);
+					
+					//ward49poly.setMap(TheMap);
+					/*
+					 * Create the user's marker and put it on the map
+					 */
+					var MarkerYou = new google.maps.Marker
+					(
+						{
+							position: MyLocation, 
+							map: TheMap,
+							draggable: true,
+							title: 'You are here.',
+							visible: true
+						}
+					);
+					/*
+					 * Bounce the marker on the map. Super cheesy.
+					 */
+					MarkerYou.setAnimation(google.maps.Animation.BOUNCE);
+					// Watch the user's device GPS for new location.
+					var Watcher = navigator.geolocation.watchPosition
+					(
+						/*
+						 * When a new position is returned to the browser...
+						 */
+						function(NewPosition)
+						{
+							/*
+							 * Move the marker to the new location.
+							 */
+							MyLocation = new google.maps.LatLng(NewPosition.coords.latitude,NewPosition.coords.longitude);
+							MarkerYou.setPosition(MyLocation);
+							TheMap.setCenter(MyLocation);
+						}, 
+						/*
+						 * Do nothing on watch position failure; just keep
+						 * listening for a new location.
+						 */
+						function() {}, 
+						/*
+						 * Set a few defaults.
+						 * enableHighAccuracy allows the location to have
+						 * better accuracy on devices that have it. No
+						 * harm if the device does not.
+						 */
+						{
+							enableHighAccuracy:true,
+							maximumAge:30000,
+							timeout:27000
+						}
+					);
+					/*
+					 * Listen for the user's click on the marker to kill the watcher and stop the animation.
+					 */
+					google.maps.event.addListener
+					(
+						MarkerYou,
+						'click',
+						function()
+						{
+							/*
+							 * Kill the Watcher object
+							 */
+							if (Watcher != false)
+							{
+								navigator.geolocation.clearWatch(Watcher);
+								Watcher = false;
+							}
+							/*
+							 * Stop the animation
+							 */
+							MarkerYou.setAnimation(null);
+						}
+					);
+					google.maps.event.addListener
+					(
+						MarkerYou,
+						'dragstart',
+						function()
+						{
+							/*
+							 * Kill the Watcher object
+							 */
+							if (Watcher != false)
+							{
+								navigator.geolocation.clearWatch(Watcher);
+								Watcher = false;
+							}
+							/*
+							 * Stop the animation
+							 */
+							MarkerYou.setAnimation(null);
+						}
+					);
+					/*
+					 * Listen for the map page to be closed and stop listening
+					 * to the user's device location service.
+					 */
+					$('#uselocation').live('click',function()
+						{
+							if(Watcher != false)
+							{
+								navigator.geolocation.clearWatch(Watcher);
+								Watcher = false;
+							}
+							/*
+							 * Test if the marker is within Chicago's 49th Ward
+							 */
+							var isWithin49 = ward49poly.containsLatLng(MarkerYou.position);
+							var geocoder = null;
+							/*
+							 * Reverse geocode the lat/lng and set the street information
+							 */
+							geocoder = new google.maps.Geocoder();
+							geocoder.geocode({'latLng': MarkerYou.position}, function(results, status) 
+								{
+									if (results[0])
+									{
+										theAddress = results[0].formatted_address;
+										if(isWithin49)
+										{
+										/*	addressArray = theAddress.split(" ");
+											$.mobile.changePage('address.php', {
+												type:'get',
+												data:'number='+addressArray[0].replace(",","")+
+													'&direction='+addressArray[1].replace(",","")+
+													'&name='+addressArray[2].replace(",","")+
+													'&suffix='+addressArray[3].replace(",",""),
+												reloadPage:true,
+												transition:'slide'
+											});
+										*/
+											alert(theAddress);
+										}
+										else
+										{
+											alert("Not in 49th Ward");
+										}
+									}
+									else
+									{
+										alert("We could not find the address of your location: " + status);
+									}
+								}
+							);
+						}
+					);
+				},
+				/*
+				 * Location Services returns an error.
+				 */
+				function(Error)
+				{
+					switch(Error.code) 
+					{
+						case Error.TIMEOUT:
+							alert ('Location services have timed out. Sorry!');
+							break;
+						case Error.POSITION_UNAVAILABLE:
+							alert ('Your location could not be determined by your device. Sorry!');
+							break;
+						case Error.PERMISSION_DENIED:
+							alert ("Your device denied location services to our web site. Check your device\'s location services settings.");
+							break;
+						default:
+							alert ('Your device reported an unknown error. Sorry!');
+							break;
+					}
+				}
+			);
+		}
+		else
+		{
+			/*
+			 * The browser does not understand geolocation
+			 */
+			alert('Your web browser is not capable of locating you.');
+		}
+	}
+});
